194569424a8db8493de5858be1c5be52c8eb7ea9
ahhyun <ahs.sunday@gmail.com>
2025-11-08 15:50:57 +0900

    refactor: register dto validation type

--- apps/server/src/auth/dto/register.dto.ts
+++ apps/server/src/auth/dto/register.dto.ts
@@ -1,16 +1,25 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterDto {
   @IsEmail()
   @IsNotEmpty()
+  @MaxLength(255)
   email: string;
 
   @IsString()
   @IsNotEmpty()
-  @MinLength(6)
+  @MinLength(8)
+  @MaxLength(128)
   password: string;
 
   @IsString()
   @IsNotEmpty()
+  @MaxLength(255)
   name: string;
 }
